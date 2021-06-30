2b9d5dcbc5e8e7e873593590a90ae82b9d510569
ing-diaz30 <84823408+ing-diaz30@users.noreply.github.com>
2021-06-29 22:03:19 -0700

    Diccionario

---
+++ Diccionario.py
@@ -0,0 +1,10 @@
+cabezeras ={
+    "Cortes":"San Pedro Sula",
+    "Olancho":"Juticalpa",
+    "Atlantida":"La Ceiba",
+    "Intibuca":"La Esperanza",
+    "Yoro":"Yoro"
+}
+
+ingresedep=input("Escriba el nombre de pais: ")
+print("La cabezera de {} es {} ".format(ingresedep,cabezeras[ingresedep]))
